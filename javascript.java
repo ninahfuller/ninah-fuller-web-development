6c007d2ab1e99a928dcc1965871940d6dbfa6395
ninahfuller <fullerninah@gmail.com>
2024-06-20 21:01:28 -0400

    update

--- javascript.java
+++ javascript.java
@@ -12,8 +12,6 @@ function validateForm() {
 </head>
 <body>
 
-<h2>JavaScript Validation</h2>
-
 <form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
   Name: <input type="text" name="fname">
   <input type="submit" value="Submit">
