863321b1853d40ff55e46cdc2522d0e2a1100d6b
ninahfuller <fullerninah@gmail.com>
2024-06-24 11:26:10 -0400

    update

--- javascript.js
+++ javascript.js
@@ -1,13 +1,7 @@
-function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = document.getElementById("numb").value;
-  // If x is Not a Number or less than one or greater than 10
-  let text;
-  if (isNaN(x) || x < 1 || x > 10) {
-    text = "Input not valid";
-  } else {
-    text = "Input OK";
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
   }
-  document.getElementById("demo").innerHTML = text;
 }
-
