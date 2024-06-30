ebeaa9829f7ebe4a25780be789e7de194e6265cc
ninahfuller <fullerninah@gmail.com>
2024-06-30 11:03:12 -0400

    update

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
-// Script.js 
-function validate() { 
-	let name = 
-		document.getElementById("name").value; 
-	let phone = 
-		document.getElementById("phone").value; 
-	let email = 
-		document.getElementById("email").value; 
-	let message = 
-		document.getElementById("message").value; 
-	let error_message = 
-		document.getElementById("error_message"); 
+const express = require('express');
 
-	error_message.style.padding = "10px"; 
+const bodyParser = require('body-parser');
 
-	let errors = []; 
+const { check, validationResult } = require('express-validator');
 
-	if (name.length < 5) { 
-		errors.push("Please Enter a valid Name");} 
-	if (isNaN(phone) || phone.length != 10) { 
-		errors.push("Please Enter a valid Phone Number");} 
-	if (email.indexOf("@") == -1 || email.length < 6) { 
-		errors.push( 
-			"Please Enter a valid Email");} 
-	if (message.length <= 40) { 
-		errors.push( 
-			"Please Enter More Than 40 Characters");} 
+const nodemailer = require('nodemailer');
 
-	if (errors.length > 0) { 
-		error_message.innerHTML = 
-			errors.join("<br>"); 
-		return false;} 
-	else { 
-		alert( 
-			"Form Submitted Successfully!"); 
-		return true;}
-    }
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+//middleware for parsing JSON in request body
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+	response.render('contact', { errors : '' });
+
+});
+
+app.post('/send', 
+	[
+		check('name').notEmpty().withMessage('Name is required'),
+		check('email').isEmail().withMessage('Invalid Email Address'),
+		check('subject').notEmpty().withMessage('Subject is required'),
+		check('message').notEmpty().withMessage('Message is required')
+	], (request, response) => {
+
+		const errors = validationResult(request);
+
+		if(!errors.isEmpty())
+		{
+			response.render('contact', { errors : errors.mapped() });
+		}
+		else
+		{
+			const transporter = nodemailer.createTransport({
+				service : 'Gmail',
+				auth : {
+					user : 'fullerninah@gmail.com',
+					pass : 'NPooh2000'
+				}
+			});
+
+			const mail_option = {
+				from : request.body.email,
+				to : 'fullerninah@gmail.com',
+				subject : request.body.subject,
+				text : request.body.message
+			};
+
+			transporter.sendMail(mail_option, (error, info) => {
+				if(error)
+				{
+					console.log(error);
+				}
+				else
+				{
+					response.redirect('/success');
+				}
+			});
+		}
+});
+
+app.get('/success', (request, response) => {
+
+	response.send('<h1>Your Message was Successfully Send!</h1>');
+
+});
+
+//start server
+app.listen(3000, () => {
+
+	console.log('Server started on port 3000');
+
+});
